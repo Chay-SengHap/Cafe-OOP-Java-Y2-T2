a3a0cfa64403ea811c71b3faec9becd54fe9654d
Chay-SengHap <chaysenghabb@gmail.com>
2026-01-20 13:54:27 +0700

    test

--- src/main/java/payroll/models/Employee.java
+++ src/main/java/payroll/models/Employee.java
@@ -1,29 +1,4 @@
 public class Employee{
-<<<<<<< HEAD
-
-    private String name;
-    private String id;
-    private double salary;
-
-    public Employee(String name, String id, double salary) {
-        this.name = name;
-        this.id = id;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-=======
     int age;
     
->>>>>>> menu
 }
